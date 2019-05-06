b48cf7012c1719e70f1e09592a18b05bad06049f
alex <temnovet@gmail.com>
2019-05-06 16:31:16 +0300

    Update strings to f-strings. Border 80 characters.

--- geoip_json/venv/create_elastic_database.py
+++ geoip_json/venv/create_elastic_database.py
@@ -7,25 +7,30 @@ from elasticsearch import Elasticsearch
 
 random.seed(datetime.datetime.now())
 
-ELASTIC_INDEX = "http://192.168.0.92:9200/"
+ELASTIC_HOST = "192.168.0.92"
+ELASTIC_PORT = 9200
 
 def get_location_info(work_ip: str = ""):
-    return requests.get("http://ip-api.com/json/" + work_ip).json()
+    return requests.get(f"http://ip-api.com/json/{work_ip}").json()
 
 
 if __name__ == "__main__":
-    es = Elasticsearch([{"host": "192.168.0.92", "port": 9200}])
+    es = Elasticsearch([{"host": ELASTIC_HOST, "port": ELASTIC_PORT}])
     es.indices.create(index="geoip", ignore=400)
-    geoip_list = []
+    geoip_info_list = []
     elastic_index: int = 1
     while (elastic_index <= 6):
-        list_for_create_random_ip = [str(random.randrange(15, 220)) for _ in range(4)]
-        current_ip = ".".join(list_for_create_random_ip)
+        random_ip_raw = [str(random.randrange(15, 220)) for _ in range(4)]
+        current_ip = ".".join(random_ip_raw)
         current_geoip_info = get_location_info(current_ip)
-        es.index(index='geoip', body=current_geoip_info, id=elastic_index, doc_type="geoipinfo")
-        geoip_list.append(current_geoip_info)
+        check_create_index = es.index(index='geoip',\
+                                      body=current_geoip_info,\
+                                      id=elastic_index, doc_type="geoipinfo")
+        print(f"index={check_create_index['_id']}",\
+              f"result={check_create_index['result']}")
+        geoip_info_list.append(current_geoip_info)
         elastic_index += 1
 
 
-    for geo_json in geoip_list:
+    for geo_json in geoip_info_list:
         print(geo_json)
